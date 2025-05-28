0f65d4ad57a7d4b836c18cd1ce64239829bc6bae
GOKAVARAPU S S D V P R REVANTH <105109654+revanth203@users.noreply.github.com>
2025-05-28 21:13:04 +0530

    Add files via upload

---
+++ Sets & Frozen sets/Day2.py
@@ -0,0 +1,41 @@
+#Numbers
+a=7
+b=3.4
+c=3+2j
+print(type(a))
+print(a)
+print(type(b))
+print(b)
+print(type(c))
+print(c)
+
+# Strings
+a="hello"
+print(type(a))
+print(a)
+
+#lists
+a=[1,2,3,4,5]
+print(type(a))
+print(a)
+
+#tuples 
+a=(1,2,4)
+print(type(a))
+print(a)
+
+#dictionaries
+a={"name":"sai", "age":20, "city":"hyderabad"}
+print(type(a))
+print(a)
+
+#sets
+a={12,35,75}
+print(type(a))
+print(a)
+
+#frozensets
+mylist = ['apple', 'banana', 'orange']
+x=frozenset(mylist)
+print(type(x))
+print(x)
