63e872e1ea85daad2604b181973d21e37f10a96a
GOKAVARAPU S S D V P R REVANTH <105109654+revanth203@users.noreply.github.com>
2025-09-12 23:29:22 +0530

    Add files via upload

---
+++ Guess a number/index.js
@@ -0,0 +1,19 @@
+let random = Math.ceil(Math.random()*100);
+let input = document.getElementById("input");
+let para  =document.getElementById("para");
+let time = document.getElementById("time");
+function guessBtn(){
+    count++;
+
+    if(input.value==random){
+        para.textContent="Congrats! Your Guessing Correct"
+        para.style.color='green'
+        time.textContent="No.Of chances: "+count;
+    }else if (input.value<random){
+        para.textContent=" Your Guessing is Too Low."
+        para.style.color='red'
+    }else{
+       para.textContent="Your  Guessing is Too High." 
+    }
+     
+}
